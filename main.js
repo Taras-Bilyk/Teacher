107030812a22b91c1b3d075bd89a3b716cef4653
Taras-Bilyk <bilyk.taras2007@gmail.com>
2022-07-01 14:08:31 +0300

    Change 1

---
+++ main.js
@@ -0,0 +1,7 @@
+var hamburger = document.querySelector(".div")
+var menu = document.querySelector(".header_menu_flex")
+
+hamburger.onclick = function(){
+  menu.classList.toggle("p2")
+  hamburger.classList.toggle("div2")
+}
